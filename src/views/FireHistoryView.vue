bac8a30f4d8545192aead76f77a7eb78c3073926
JeewanthaSenanayake <jeewanthamsenanayake@gmail.com>
2024-10-26 16:08:49 +0530

    add gradient ball

--- src/views/FireHistoryView.vue
+++ src/views/FireHistoryView.vue
@@ -3,9 +3,16 @@
         <NavigatorComponent :pageName="`Fire History`" />
         <v-container class="svg-background">
             <svg class="background-svg" viewBox="0 0 500 500">
-                <circle cx="100" cy="100" r="80" fill="rgba(255, 69, 0)" />
-                <circle cx="400" cy="150" r="100" fill="rgba(255, 255, 0)" />
-                <circle cx="250" cy="400" r="150" fill="rgba(255, 165, 0)" />
+                <defs>
+                    <linearGradient id="gradient1" x1="0%" y1="0%" x2="100%" y2="100%">
+                        <stop offset="30%" style="stop-color: #28A745; stop-opacity: 1" /> <!-- Green -->
+                        <stop offset="60%" style="stop-color: #FFA500; stop-opacity: 1" /> <!-- Orange -->
+                        <stop offset="90%" style="stop-color: #007BFF; stop-opacity: 1" /> <!-- Blue -->
+                    </linearGradient>
+                </defs>
+                <circle cx="100" cy="10" r="100" fill="url(#gradient1)" />
+                <circle cx="575" cy="55" r="150" fill="url(#gradient1)" />
+                <circle cx="275" cy="400" r="150" fill="url(#gradient1)" />
             </svg>
             <v-card style="background-color: rgba(66, 66, 66, 0.7); border-radius: 15px; height: 100vh;" class="pa-2">
                 <h1 class="text-center my-3" style="color: white;">Fire Dashboard</h1>
